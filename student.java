6920a4dfb2a63475577c57fb9c5e4d79a26eb97e
angievininggrau <156046842+angievininggrau@users.noreply.github.com>
2024-02-18 20:50:33 -0800

    Create student.java

---
+++ student.java
@@ -0,0 +1,18 @@
+public class App {
+    public static void main(String[] args) {
+
+        Student myStudent = new Student();
+        myStudent.firstName = "Angie";
+        myStudent.lastName = "Vining";
+        myStudent.major = "Computer Science";
+        myStudent.gpa = 3.5;
+        myStudent.age = 24;
+        myStudent.city = "Fresno";
+        myStudent.onProbation = false;
+
+        System.out.println(myStudent.firstName);
+
+
+    }
+
+}
